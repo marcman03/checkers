2b6a55bf546e097176bfe9bfc0bc87955c49239e
marcman03 <142247506+marcman03@users.noreply.github.com>
2023-12-15 12:31:09 +0100

    Update ElMeuStatus.java (update con move) NOSE(Diferenciador)
    He añadido el update cuando se mueve una ficha no he comprovado si va el diferenciador

--- Pascualinho/src/edu/upc/epsevg/prop/checkers/players/ElMeuStatus.java
+++ Pascualinho/src/edu/upc/epsevg/prop/checkers/players/ElMeuStatus.java
@@ -3,7 +3,11 @@ package edu.upc.epsevg.prop.checkers.players;
 
 import edu.upc.epsevg.prop.checkers.CellType;
 import edu.upc.epsevg.prop.checkers.GameStatus;
+import edu.upc.epsevg.prop.checkers.PlayerMove;
 import edu.upc.epsevg.prop.checkers.PlayerType;
+import edu.upc.epsevg.prop.checkers.SearchType;
+import java.awt.Point;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -12,58 +16,121 @@ import java.util.Random;
  */
 public class ElMeuStatus extends GameStatus {
     private static final long[][][] zobristTable = new long[GameStatus.N][GameStatus.N][4];
-    private static long diferenciador;
+    private static final long[][] zobristTableDiferenciador = new long[GameStatus.N][GameStatus.N];
+    private long hash;
     static {
         // Inicializar la tabla Zobrist con valores aleatorios
         Random random = new Random();
-        diferenciador =random.nextLong();
+        
         for (int i = 0; i < GameStatus.N; ++i) {
             for (int j = 0; j < GameStatus.N; ++j) {
                 for (int k = 0; k < 4; ++k) {
                     zobristTable[i][j][k] = random.nextLong();
                 }
+             zobristTableDiferenciador[i][j] = random.nextLong();
             }
         }
     }
     public ElMeuStatus(int [][] tauler){
         super(tauler);
+       
     }
     
      public ElMeuStatus(GameStatus gs){
         super(gs);
+        PlayerType player=gs.getCurrentPlayer();
+        hash= CalculateHash(player);
         
     }
      
+    @Override
+    public void movePiece(List<Point> move) {
+        PlayerType player= getCurrentPlayer();
+        updateHash(move, player);
+        super.movePiece(move);
+
+        // Actualiza el hash después de realizar el movimiento.
+     
+    }
+    private void updateHash(List<Point>move,PlayerType player){
+        Point firstPoint=move.get(0);
+        for(int i=0; i<move.size();++i){
+            Point currentPoint=move.get(i);
+            int x= currentPoint.x;
+            int y= currentPoint.y;
+            Point capture=null;
+            Point nextPoint=null;
+            int nextX=0;
+            int nextY=0;
+            if (i< move.size()-1){
+                nextPoint =move.get(i+1);
+                nextX= nextPoint.x;
+                nextY= nextPoint.y;
+                capture= detectCapture(x,y,nextX,nextY);
+            }
+            if (nextPoint!=null){
+                hash ^=zobristTable[x][y][getPieceIndex(getPos(firstPoint.x, firstPoint.y))];// No estoy seguro si hay un problema si se convierte en reina justo
+                if (player == PlayerType.PLAYER1) hash ^= zobristTableDiferenciador[x][y];
+                hash ^=zobristTable[nextX][nextY][getPieceIndex(getPos(firstPoint.x, firstPoint.y))];
+                if (player == PlayerType.PLAYER1) hash ^= zobristTableDiferenciador[nextX][nextY];
+                //si existe una captura
+                if (capture!= null){
+                    hash ^=zobristTable[capture.x][capture.y][getPieceIndex(getPos(capture.x, capture.y))];
+                    if (player == PlayerType.PLAYER1) hash ^= zobristTableDiferenciador[capture.x][capture.y];
+                }       
+            }
+        }
+    }
+    //comprovar funcion
+    private Point detectCapture(int x, int y, int nextX, int nextY){
+        if (Math.abs(nextX - x) == 2 && Math.abs(nextY - y) == 2) {
+            int capturedX = (x +nextX) / 2;
+            int capturedY = (y + nextY) / 2;
+            return(new Point(capturedX,capturedY));
+        }
+        return null;
     
-    public long getHash(PlayerType player) {
+    
+    
+    
+    }
+    private int getPieceIndex(CellType type) {
+        if (type == CellType.P1) {
+            return 0;
+        } else if (type == CellType.P1Q) {
+            return 1;
+        } else if (type == CellType.P2) {
+            return 2;
+        } else if (type == CellType.P2Q) {
+            return 3;
+        }
+        return -1; // Valor por defecto o error.
+    }
+    
+    
+    private long CalculateHash(PlayerType player) {
         long h = 0;
         for (int y = 0; y < getSize(); ++y) {
             for (int x = 0; x < getSize(); ++x) {
                 CellType type = getPos(x, y);
                
                 if (type != CellType.EMPTY) {
-                    int pieza=0;
-                    if (type == CellType.P1) {
-                        pieza = 0;
-                    } else if (type == CellType.P1Q) {
-                        pieza = 1;
-                    } else if (type == CellType.P2) {
-                        pieza = 2;
-                    } else if (type == CellType.P2Q) {
-                        pieza = 3;
-                    }
-                    h ^= zobristTable[x][y][pieza];
                     
-                   
-                    
+                    h ^= zobristTable[x][y][getPieceIndex(type)];
+                    if (player == PlayerType.PLAYER1) h ^= zobristTableDiferenciador[x][y];
                 }
             }
         }
-        if (player == PlayerType.PLAYER1) {
-            h ^= diferenciador;
-        }
+        
         return h;
     }
+    
+    
+    public long getHash(){
+      return hash;
+    
+    }
+
     
     
 }
